ae82090bad2453705c30a0d9971ab0a695b08a72
PhamVanDiep <phamdiepa1k55@gmail.com>
2021-12-23 07:19:30 +0700

    update up black background image

--- knn/Main.py
+++ knn/Main.py
@@ -20,9 +20,9 @@ height = 480
 img = cv2.imread("digits.png",0)
 cells = [np.hsplit(row, 100) for row in np.vsplit(img, 50)]
 x = np.array(cells) 
-train = x[:,:50].reshape(-1,400).astype(np.float32)
+train = x[:,:100].reshape(-1,400).astype(np.float32)
 k = np.arange(10)
-train_labels = np.repeat(k,250)[:,np.newaxis]
+train_labels = np.repeat(k,500)[:,np.newaxis]
 knn = cv2.ml.KNearest_create()
 knn.train(train,0,train_labels)
 
@@ -123,6 +123,58 @@ def upload_image(): #Hàm upload với ảnh có nền đen
     for img in glob.glob(fileName): #Lấy file ảnh từ đường dẫn và lưu ảnh vào biến image
         image = cv2.imread(img, 0)
 
+    ret, th = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    #chuyển giá trị màu của ảnh về hai màu là trắng hoặc đen(giá trị 0 hoặc 255, không có giá trị xám(VD 1 2 3 125 về 0, 128 129 254 về 255))
+
+    contours = cv2.findContours(th, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+    #Xác định những số có trong ảnh bằng cách tìm những mã màu 255(trắng) đứng cạnh nhau và cho nó vào một khung hình chữ nhật
+
+    f = open('text.txt', 'w+')
+    #Mở file để ghi những số nhận diện được
+    count = 0
+
+    for cnt in contours:
+
+        x, y, w, h = cv2.boundingRect(cnt)
+        
+        cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 1)
+        digit = th[y:y + h, x:x + w]
+        resized_digit = cv2.resize(digit, (20, 20))
+        x2 = np.array(resized_digit)
+        print(x2)
+        test2 = resized_digit.reshape(-1,400).astype(np.float32)
+        result1, result2, result3, result4 = knn.findNearest(test2, 10)
+        print(result1)
+        print(result2)
+        print(result3)
+        print(result4)
+
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        fontScale = 0.5
+        color = (255, 0, 0)
+        thickness = 1
+        cv2.putText(image, str(int(result1)), (x, y - 5), font, fontScale, color, thickness)
+        f.write(str(int(result1)))
+        count += 1
+
+    print("Có tất cả:", count,"số được nhận diện")
+    f.close()
+    f = open('text.txt', 'r')
+    data1 = f.read()
+    print("Những số nhận diện được:",data1)
+    print("---------------------------------------------------")
+    
+    cv2.imshow('Ket qua phan tich', image)
+    cv2.waitKey(0)
+
+def upload_image1(): #Hàm upload với ảnh có nền đen
+    a = []
+    fileName = filedialog.askopenfilename(initialdir = "/", title="Select A File",filetype=(("all","*.*"),("jpeg","*.jpg"),("png","*.png")))
+    #Lấy đường dẫn của ảnh cần đọc
+
+    for img in glob.glob(fileName): #Lấy file ảnh từ đường dẫn và lưu ảnh vào biến image
+        image = cv2.imread(img, 0)
+
     image = ~image
     ret, th = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
     #chuyển giá trị màu của ảnh về hai màu là trắng hoặc đen(giá trị 0 hoặc 255, không có giá trị xám(VD 1 2 3 125 về 0, 128 129 254 về 255))
@@ -175,5 +227,7 @@ button_clear = Button(text='Xoá', command=clear_widget)
 button_clear.grid(row=2, column=1, pady=1, padx=1)
 btn_save = Button(text='Tải ảnh lên', command=upload_image)
 btn_save.grid(row=2, column=2, pady=1, padx=1)
+btn_save = Button(text='Ảnh nền đen', command=upload_image1)
+btn_save.grid(row=2, column=3, pady=1, padx=1)
 
 root.mainloop() #Hàm chính giúp giao diện được hiển thị và chương trình luôn chạy

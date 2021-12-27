9a0bd94f36017ce24bbcca448a9fd28d4ffdd841
PhamVanDiep <phamdiepa1k55@gmail.com>
2021-12-27 15:10:41 +0700

    Update Main.py

--- knn/Main.py
+++ knn/Main.py
@@ -6,9 +6,8 @@ import numpy as np
 from PIL import ImageGrab
 from matplotlib import pyplot as plt
 from mnist import MNIST
-import os
 
-#khởi tạo GUI
+# 11 - 18: khởi tạo GUI
 root = Tk() 
 root.geometry('648x516+0+0')
 root.resizable(0, 0)
@@ -20,8 +19,7 @@ height = 480
 
 #khởi tạo dữ liệu training
 
-#This dataset is already split into training data in the form of a 2D list of integers.
-#mnist = MNIST('../dataset/MNIST')
+# dataset này đã được training thành mảng các số nguyên 2 chiều
 mnist = MNIST()
 images_train, labels_train = mnist.load_training()
 
@@ -39,7 +37,7 @@ k = 3 #khởi tạo số lượng hàng xóm
 cv = Canvas(root, width=width, height=height, bg='white')
 cv.grid(row=0, column=0, pady=2, sticky=W, columnspan=4)
 
-#khởi tạo font
+# 41 - 44: khởi tạo font
 font = cv2.FONT_HERSHEY_SIMPLEX
 fontScale = 0.5
 color = (255, 0, 0)
@@ -50,7 +48,7 @@ def clear_widget(): #Hàm xoá toàn bộ những gì đã vẽ
     cv.delete('all')
     print("Đã xoá")
     print("---------------------------------------------------")
-
+# 52 - 66 để vẽ lên bảng
 lastx, lasty = None, None
 
 def draw_lines(event):
@@ -66,153 +64,93 @@ def activate_event(event):
 
 cv.bind('<Button-1>', activate_event)
 
-def put_text_result(image, count):
+#in kết quả ra console
+def put_text_result(count):
     print("Có tất cả:", count,"số được nhận diện")
     f = open('text.txt', 'r')
     data1 = f.read()
     print("Những số nhận diện được: ", data1)
     print("---------------------------------------------------")
-    cv2.imshow('OCR', image)
     cv2.waitKey(0)
 
-def Recognize_Digit(): #Nhận diện số trên bảng
-    a = []
-    filename = f'temp.png' #Lưu lại bảng với tên file: temp.png
+# Hàm nhận diện ảnh với tham số truyền vào là một số
+# Số 1 là để hàm thực hiện nhận diện số khi vẽ lên bảng
+# Số 2 là để hàm thực hiện nhận diện số trong ảnh nền trắng
+# Số 3 là để hàm thực hiện nhận diện số trong ảnh nền đen
+def Recognize_Digit(type_img):
+    # 82 - 90: lưu ảnh cần nhận diện vào biến image 
+    if(type_img != 1):
+        #Lấy đường dẫn của ảnh cần đọc
+        fileName = filedialog.askopenfilename(initialdir = "/", title="Select A File",filetype=(("all","*.*"),("jpeg","*.jpg"),("png","*.png")))        
+        for img in glob.glob(fileName): #Lấy file ảnh từ đường dẫn và lưu ảnh vào biến image
+            image = cv2.imread(img, cv2.IMREAD_COLOR)    
+    else: 
+        filename = f'temp.png' # Lưu lại bảng với tên file: temp.png
+        image = cv2.imread(filename, cv2.IMREAD_COLOR)
+
+    if(type_img == 3): image = ~image #Nếu là ảnh nền đen thì đưa nó về ảnh nền trắng chữ đen
+
+    gray = cv2.cvtColor(image.copy(), cv2.COLOR_BGR2GRAY)
+    tmp, th = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    # chuyển giá trị màu của ảnh về hai màu là trắng hoặc đen(giá trị 0 hoặc 255, 
+    # không có giá trị xám(VD 1 2 3 125 về 0, 128 129 254 về 255))
+
+    contours = cv2.findContours(th, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+    # Xác định những số có trong ảnh bằng cách tìm những mã màu 255(trắng), 
+    # đứng cạnh nhau và cho nó vào một khung hình chữ nhật
+
+    f = open('text.txt', 'w+')
+    count = 0
+
+    for cnt in contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+        if(w > 2 and h > 6):
+            # 109 - 112: resize ảnh về kích thước 28x28 và đưa về mảng 1x784 kiểu float
+            cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 1)
+            digit = th[y:y + h, x:x + w]
+            resized_digit = cv2.resize(digit, (18, 18))
+            padded_digit = np.pad(resized_digit, ((5, 5), (5, 5)), "constant", constant_values=0)
+            img = np.array(padded_digit)
+            rec_img = img.reshape(-1,28*28).astype(np.float32)
+
+            #Bắt đầu thực hiện tìm hàng xóm gần nhất(với k hàng xóm)
+            return_value, results, neighbors, distances = knn.findNearest(rec_img, k)
+            result = str(results.astype(int)[0][0]) # ép kiểu cho kết quả trả về ra số nguyên và -> kiểu string 
+            print(result)       # in ra kết quả của số được nhận dạng
+            print(neighbors)    # in ra các hàng xóm gần nhất với số lượng đã được định nghĩa ở trên
+            print(distances)    # in ra khoảng cách tương ứng từ mục tiêu tới các hàng xóm
+
+    # 123 - 128: lưu kết quả và hiển thị kết quả ra màn hình + console
+            cv2.putText(image, result, (x, y - 5), font, fontScale, color, thickness) 
+            f.write(result)
+            count += 1
+    cv2.imshow('OCR', image)
+    f.close()                   
+    put_text_result(count)
+
+def OCR_on_table(): # Nhận diện số trên bảng
+    # 133 - 143: crop bảng và lưu vào file temp.png
+    filename = f'temp.png'
     widget = cv
     
     x = widget.winfo_rootx() + 5
     y = widget.winfo_rooty() + 5
-    x1 = x + widget.winfo_width() + 155 
+    x1 = x + widget.winfo_width() + 155  # Dành cho máy độ phân giải 1920x1080
     y1 = y + widget.winfo_height() + 120
-
-    # x1 = widget.winfo_width() #Dành cho máy độ phân giải 1366x768
+    # x1 = widget.winfo_width() # Dành cho máy độ phân giải 1366x768
     # y1 = widget.winfo_height()
 
     ImageGrab.grab().crop((x, y, x1, y1)).save(filename) #Lưu ảnh mình vẽ được lại
+    Recognize_Digit(1)
+    
+def upload_image(): # Hàm upload với ảnh có nền trắng
+    Recognize_Digit(2)
 
-    image = cv2.imread(filename, cv2.IMREAD_COLOR)
-    gray = cv2.cvtColor(image.copy(), cv2.COLOR_BGR2GRAY)
-    ret, th = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-    contours = cv2.findContours(th, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
-
-    f = open('text.txt', 'w+')
-
-    count = 0
-
-    for cnt in contours:
-
-        x, y, w, h = cv2.boundingRect(cnt)
-        if(w > 2 and h > 6):
-            cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 1)
-            digit = th[y:y + h, x:x + w]
-            resized_digit = cv2.resize(digit, (18, 18))
-            padded_digit = np.pad(resized_digit, ((5, 5), (5, 5)), "constant", constant_values=0)
-
-            img = np.array(padded_digit)
-            rec_img = img.reshape(-1,28*28).astype(np.float32)
-
-            #Bắt đầu thực hiện tìm hàng xóm gần nhất(với k hàng xóm)
-            return_value, results, neighbors, distances = knn.findNearest(rec_img, k)
-            result = str(results.astype(int)[0][0])
-            print(result)
-            print(neighbors)
-
-            cv2.putText(image, result, (x, y - 5), font, fontScale, color, thickness)
-            f.write(result)
-            count += 1
-
-    f.close()    
-    put_text_result(image, count)
-
-
-
-
-def upload_image(): #Hàm upload với ảnh có nền trắng hoặc xám
-    a = []
-    fileName = filedialog.askopenfilename(initialdir = "/", title="Select A File",filetype=(("all","*.*"),("jpeg","*.jpg"),("png","*.png")))
-    #Lấy đường dẫn của ảnh cần đọc
-
-    for img in glob.glob(fileName): #Lấy file ảnh từ đường dẫn và lưu ảnh vào biến image
-        image = cv2.imread(img, 0)
-
-    ret, th = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-    #chuyển giá trị màu của ảnh về hai màu là trắng hoặc đen(giá trị 0 hoặc 255, không có giá trị xám(VD 1 2 3 125 về 0, 128 129 254 về 255))
-
-    contours = cv2.findContours(th, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
-    #Xác định những số có trong ảnh bằng cách tìm những mã màu 255(trắng) đứng cạnh nhau và cho nó vào một khung hình chữ nhật
-
-    f = open('text.txt', 'w+')
-    #Mở file để ghi những số nhận diện được
-    count = 0
-
-    for cnt in contours:
-
-        x, y, w, h = cv2.boundingRect(cnt)
-        if(w > 2 and h > 6):
-            cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 1)
-            digit = th[y:y + h, x:x + w]
-            resized_digit = cv2.resize(digit, (18, 18))
-            padded_digit = np.pad(resized_digit, ((5, 5), (5, 5)), "constant", constant_values=0)
-            x2 = np.array(padded_digit)
-
-            img = np.array(padded_digit)
-            rec_img = img.reshape(-1,28*28).astype(np.float32)
-
-            #Bắt đầu thực hiện tìm hàng xóm gần nhất(với k hàng xóm)
-            return_value, results, neighbors, distances = knn.findNearest(rec_img, k)
-            result = str(results.astype(int)[0][0])
-            print(result)
-            print(neighbors)
-
-            cv2.putText(image, result, (x, y - 5), font, fontScale, color, thickness)
-            f.write(result)
-            count += 1
-
-    f.close()
-    put_text_result(image, count)
-
-def upload_black_background_image(): #Hàm upload với ảnh có nền đen
-    #176-190 tương tự hàm bên trên
-    a = []
-    fileName = filedialog.askopenfilename(initialdir = "/", title="Select A File",filetype=(("all","*.*"),("jpeg","*.jpg"),("png","*.png")))
-    for img in glob.glob(fileName):
-        image = cv2.imread(img, 0)
-    image = ~image
-    ret, th = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-    contours = cv2.findContours(th, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
-
-    f = open('text.txt', 'w+')
-
-    count = 0
-
-    for cnt in contours:
-
-        x, y, w, h = cv2.boundingRect(cnt)
-        if(w > 2 and h > 6):
-            cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 1)
-            digit = th[y:y + h, x:x + w]
-            resized_digit = cv2.resize(digit, (18, 18))
-            padded_digit = np.pad(resized_digit, ((5, 5), (5, 5)), "constant", constant_values=0)
-            x2 = np.array(padded_digit)
-
-            img = np.array(padded_digit)
-            rec_img = img.reshape(-1,28*28).astype(np.float32)
-
-            #Bắt đầu thực hiện tìm hàng xóm gần nhất(với k hàng xóm)
-            return_value, results, neighbors, distances = knn.findNearest(rec_img, k)
-            result = str(results.astype(int)[0][0])
-            print(result)
-            print(neighbors)
-
-            cv2.putText(image, result, (x, y - 5), font, fontScale, color, thickness)
-            f.write(result)
-            count += 1
-
-    f.close()
-    put_text_result(image, count)
+def upload_black_background_image(): # Hàm upload với ảnh có nền đen
+    Recognize_Digit(3)
 
 #4 button trong giao diện
-btn_save = Button(text='Nhận dạng', command = Recognize_Digit)
+btn_save = Button(text='Nhận dạng', command = OCR_on_table)
 btn_save.grid(row=2, column=0, pady=1, padx=1)
 button_clear = Button(text='Xoá', command = clear_widget)
 button_clear.grid(row=2, column=1, pady=1, padx=1)
